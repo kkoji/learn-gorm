12cb45ad2740b2da5ebf4f502a59eec1ea7a701f
Kazuya Kojima <kkojima@improver.co.jp>
2023-10-22 19:59:32 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price int
+}
+
+func main() {
+	dsn := "root:example@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	err = db.AutoMigrate(&Product{})
+	if err != nil {
+		panic("failed to migrate product")
+	}
+}
